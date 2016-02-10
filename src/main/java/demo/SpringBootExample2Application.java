fd5b7e8ee618b8fefc4e3a681c6da82641ac26b3
ajmalch <ajmalch@gmail.com>
2016-02-09 20:14:32 -0500

    updated code

--- src/main/java/demo/SpringBootExample2Application.java
+++ src/main/java/demo/SpringBootExample2Application.java
@@ -3,19 +3,28 @@ package demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 @EnableAutoConfiguration
 public class SpringBootExample2Application {
+	
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootExample2Application.class, args);
+    }
 
-	@RequestMapping("/")
+    @RequestMapping("/")
 	String sayHello(){
 		return "Hello World";
 	}
-    public static void main(String[] args) {
-        SpringApplication.run(SpringBootExample2Application.class, args);
+	
+    @RequestMapping(value="/hello/{name}")
+    @ResponseBody
+    String sayHi( @PathVariable String name){
+    	return "Hi " + name;
     }
 }
